8cb31b99456fc403fab0a514d102160089601638
peter.tran <peter.tran@sainsburys.co.uk>
2020-05-27 20:19:28 +0100

    Lib mobile number test on AWS Lambda

--- src/main/java/lamba/mobiletest/App.java
+++ src/main/java/lamba/mobiletest/App.java
@@ -2,12 +2,27 @@ package lamba.mobiletest;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.Phonenumber;
+import org.apache.commons.validator.routines.EmailValidator;
 
 public class App implements RequestHandler<Object, Object> {
 
     @Override
     public Object handleRequest(Object input, Context context) {
         System.out.println("Hello World!");
+
+        boolean validEmail = EmailValidator.getInstance().isValid("peter.tran@sainsburys.co.uk");
+        System.out.println(validEmail);
+
+        PhoneNumberUtil phoneNumberUtil = PhoneNumberUtil.getInstance();
+        try {
+            Phonenumber.PhoneNumber no = phoneNumberUtil.parse("07471149533", "GB");
+            System.out.println(no.getNationalNumber());
+        } catch (NumberParseException e) {
+            System.out.print(e);
+        }
         return null;
     }
 }
